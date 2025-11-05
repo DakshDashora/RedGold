062667bd8f83bb4da1487b9335b68071908f2c30
DakshDashora <dakshdashora11@gmail.com>
2025-11-05 22:18:12 +0530

    Refactor database connection to use environment variable

--- Backend/database.py
+++ Backend/database.py
@@ -1,13 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine= create_engine("sqlite:///./redGold.db" , connect_args={"check_same_thread":False})
-sessionLocal= sessionmaker(bind=engine, autoflush=False, autocommit=False)
-Base=declarative_base()
+DATABASE_URL = os.getenv("DATABASE_URL")  # Render env var (your Neon URL)
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
 
 def get_db():
-    db=sessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
