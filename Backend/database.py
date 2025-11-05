3a885cbbef795610bc410198aa87e0380f28a4c6
DakshDashora <dakshdashora11@gmail.com>
2025-11-05 22:36:41 +0530

    change

--- Backend/database.py
+++ Backend/database.py
@@ -10,7 +10,7 @@ SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
+    db = sessionLocal()
     try:
         yield db
     finally:
