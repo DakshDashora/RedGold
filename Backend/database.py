ba07a0ff42f931a42a834f911348c9df3926eb5a
DakshDashora <dakshdashora11@gmail.com>
2025-11-05 22:37:55 +0530

    change 2

--- Backend/database.py
+++ Backend/database.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = os.getenv("DATABASE_URL")  # Render env var (your Neon URL)
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
 def get_db():
